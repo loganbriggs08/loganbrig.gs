1d83ca50d434451212847a7a4eacff43e84c6883
loganbriggs08 <122122838+loganbriggs08@users.noreply.github.com>
2025-02-22 20:52:17 +0000

    Generate base code

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className="">
+            <button className="btn btn-primary">
+                Hello World
+            </button>
+		</div>
+	);
+}
